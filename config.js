68e03651d0deccee4cef14cc7bb11aae5d244a5e
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2024-06-19 22:15:00 -0300

    ajuste endpoint

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 
 const config = {
     cor: "#eee",
-    img: 'img/logo.png'
+    img: 'img/logo.png',
+    endpoint: 'https:6a2f5596-8679-4f48-9fe3-68883864c158-00-z3wki8twje2e.janeway.replit.dev/'
 };
 
 
